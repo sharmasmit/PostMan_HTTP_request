4ccba5f8c1e239aa5853d54766e4ede79c135329
sharmasmit <smeet.sharma007@gmail.com>
2024-03-26 11:49:22 +0530

    Some Changes Added

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Good Morning</h1>");
+});
+
+app.post("/ragister", (req, res) => {
+  res.sendStatus(201);
+});
+
+app.put("/user/smit", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.patch("/user/smit", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete("/user/smit", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Local Started on Port ${port}`);
+});
